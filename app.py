76661853fb9edf3c3dc0e2abf0e8f6022042406a
Thiago Zani <thiago.zani@uol.com.br>
2022-09-26 21:37:51 -0300

    Upload atividade Desenvolvimento Wbe I em Flask - Desafio 3

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask("__name__")
+
+@app.route('/')
+def index(): 
+    return render_template('index.html')
+
+@app.route('/quem_somos')
+def quem_somos():
+    return render_template('quem_somos.html')
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
