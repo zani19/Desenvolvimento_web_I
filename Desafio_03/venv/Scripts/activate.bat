1312b03f43d73642f779403221d6c4e4fecd3b37
Thiago Zani <thiago.zani@uol.com.br>
2022-11-18 14:37:57 -0300

    Atualizacao VENV

--- Desafio_03/venv/Scripts/activate.bat
+++ Desafio_03/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\FATEC\Desenvolvimento Web I\venv
+set VIRTUAL_ENV=D:\FATEC\APIs\Desenvolvimento_web_I\Desafio_03\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
